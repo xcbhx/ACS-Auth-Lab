af73f81789139d352645e28bb1292ea9d2688a90
Meredith Murphy <meredithcat@gmail.com>
2021-02-11 09:40:13 -0500

    update starter code

--- books_app/__init__.py
+++ books_app/__init__.py
@@ -17,17 +17,7 @@ db = SQLAlchemy(app)
 
 # TODO: Add authentication setup code here!
 
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
-login_manager.init_app(app)
 
-from .models import User
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-bcrypt = Bcrypt(app)
 
 ###########################
 # Blueprints
