056ce857b3d68d5b34b9864ffaa692972bc11fbc
Mitchell Hudson <soggybag@gmail.com>
2022-04-11 13:20:56 -0700

    Update models.py

--- books_app/models.py
+++ books_app/models.py
@@ -1,5 +1,5 @@
 """Create database models to represent tables."""
-from books_app.extensions import db
+from books_app import db
 from sqlalchemy.orm import backref
 from flask_login import UserMixin
 import enum
